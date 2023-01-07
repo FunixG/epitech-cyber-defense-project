7198b848f474f3748cb7434f139da3bd6a8179af
Antoine PRONNIER (Funix) <antoine.pronnier@gmail.com>
2023-01-07 11:42:32 +0100

    Dns done, need testing

--- scripts/dns/setupDns.sh
+++ scripts/dns/setupDns.sh
@@ -14,11 +14,20 @@ echo "127.0.0.1 powerzio.net" | sudo tee -a "/etc/hosts" > /dev/null
 
 cat ./configs/db.powerzio.net | sudo tee "/etc/bind/db.powerzio.net" > /dev/null
 
+#Dns principal
 echo \
  "zone \"powerzio.net\" {\
   type master;\
   file \"/etc/bind/db.powerzio.net\";\
 };" | sudo tee -a "/etc/bind/named.conf.local"
+
+#Reverse dns
+echo \
+  "zone \"0.10.10.in-addr.arpa\" {\
+  type master;\
+  notify no;
+  file \"/etc/bind/db.10\";\
+};" | sudo tee "/etc/bind/named.conf.local"
 
 sudo systemctl restart bind9
 sudo systemctl enable bind9
